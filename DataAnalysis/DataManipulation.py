3e3eb7d19c605916097d8ca58343bfab497b2498
James Bevins <james.e.bevins@gmail.com>
2017-03-09 00:20:06 -0800

    Added a bin differentiation function

--- DataAnalysis/DataManipulation.py
+++ DataAnalysis/DataManipulation.py
@@ -68,6 +68,62 @@ def bin_integration(edges, data, edgeLoc="low"):
     return f
 
 #------------------------------------------------------------------------------#
+def bin_differentiation(edges, data, edgeLoc="low"):
+    """!
+    @ingroup DataManipulation
+
+    Integrates binned data.  Can be used, for example, to convert from
+    differential flux to flux.  Valid for binned data with edge or midpoint
+    values if properly specified in the inputs.
+
+    @param edges: <em> list or array of floats </em> \n
+        The lower or upper bin energies.  This list should have a size that is
+        one greater than the size of the data. \n
+    @param data: <em> list or array of floats </em> \n
+        The data corresponding to the bin structure.  This should be of
+        len(edges)-1. \n
+    @param edgeLoc: \e string \n
+        Indicator for the location of the energy boundary edges.  Options
+        are "low", "mid", or "up". \n
+
+    @return <em> list of floats </em>: The differentiated data
+    """
+
+    assert edgeLoc == "low" or edgeLoc == "mid" or edgeLoc == "up", \
+      "Valid specifications for the edge location are 'low', 'mid', or 'up'."
+
+    f = []
+
+    # Check for expected data consistency
+    if edgeLoc == "low":
+        if len(edges) == len(data):
+            try:
+                edges.append(edges[-1]+(edges[-1]-edges[-2]))
+            except TypeError:
+                edges = edges.tolist()
+                edges.append(edges[-1]+(edges[-1]-edges[-2]))
+    if edgeLoc == "up":
+        if len(edges) == len(data):
+            try:
+                edges.insert(0, 0)
+            except TypeError:
+                edges = edges.tolist()
+                edges.insert(0, 0)
+    if edgeLoc == "mid":
+        tmp = []
+        width = edges[1]-edges[0]
+        tmp.append(edges[0]-0.5*width)
+        for i in range(0, len(edges)):
+            tmp.append(edges[i]+0.5*width)
+        edges = tmp
+
+    # Integrate according to the type of binning provided
+    for i in range(0, len(data)):
+        f.append(data[i]/(edges[i+1]-edges[i]))
+
+    return f
+
+#------------------------------------------------------------------------------#
 def normAUBC(data):
     """!
     @ingroup DataManipulation
