a9050aa9a36ff4bbbd71ccff109914cd7b3bbd12
James Bevins <james.e.bevins@gmail.com>
2017-03-13 10:42:15 -0700

    Added beam dead time models

--- GeneralNuclear/Detectors.py
+++ GeneralNuclear/Detectors.py
@@ -16,7 +16,7 @@ Tools and methods that are generally useful in characterizing detector systems.
 from math import exp
 
 #------------------------------------------------------------------------------#
-def nonparalyzableDeadTime(obsCountRate, deadTime):
+def nonparalyzable_beam_dead_time(obsCountRate, deadTime):
     """!
     @ingroup Detectors
     Calculates the true count rate given a measured count rate and the known
@@ -30,12 +30,15 @@ def nonparalyzableDeadTime(obsCountRate, deadTime):
         System dead time in untis of [s]  \n
 
     @return float: The actual interaction rate \n
+            float: The fractional dead time \n
     """
 
-    return obsCountRate/(1-obsCountRate*deadTime)
+    trueRate = obsCountRate/(1.0-obsCountRate*deadTime)
+    deadTime = (trueRate-obsCountRate)/float(trueRate)
+    return trueRate, deadTime
 
 #------------------------------------------------------------------------------#
-def paralyzableDeadTime(obsCountRate, deadTime):
+def paralyzable_dead_time(obsCountRate, deadTime):
     """!
     @ingroup Detectors
     Calculates the true count rate given a measured count rate and the known
@@ -51,6 +54,7 @@ def paralyzableDeadTime(obsCountRate, deadTime):
         System dead time in untis of [s]  \n
 
     @return float: The actual interaction rate \n
+            float: The fractional dead time \n
     """
 
     # Create initial guess
@@ -60,4 +64,94 @@ def paralyzableDeadTime(obsCountRate, deadTime):
     while abs(trueRate*exp(-trueRate*deadTime) - obsCountRate) > 1:
         trueRate += 1
 
-    return trueRate
+    deadTime = (trueRate-obsCountRate)/float(trueRate)
+    return trueRate, deadTime
+
+#------------------------------------------------------------------------------#
+def nonparalyzable_beam_dead_time(obsCountRate, deadTime, beamPeriod):
+    """!
+    @ingroup Detectors
+    Calculates the true count rate and dead time fraction using a nonparalyzable
+    model given a measured count rate and the known system dead time according
+    to the bunched synchrotron radiation dead time model from: "Success and
+    failure of dead- time models as applied to hybrid pixel detectors in
+    high-flux applications."
+
+    \f$ N_{out} = \frac{[1- \exp(-N_{in}* \tau_b]/ \tau_b}
+                       {1+[1- \exp(-N_{in}* \tau_b]*n} \f$
+
+    where
+
+    \f$ n=Int( \frac{\tau_s}{\tau_b}) \f$
+
+    This is solved iteratively since \f$N_{in}\f$ cannot be solved for
+    exlicitly.
+
+    @param obsCountRate: <em> integer or float </em> \n
+        \f$N_{out}\f$: The recorded count rate for the system in units of
+        [\f$s^{-1}\f$] \n
+    @param deadTime: <em> integer or float </em>  \n
+        \f$\tau_s\f$: System dead time in untis of [s]  \n
+    @param beamPeriod: <em> integer or float </em>  \n
+        \f$\tau_b\f$: The time between beam bunches in untis of [s]  \n
+
+    @return float: \f$N_{in}\f$: The actual interaction rate \n
+            float: The fractional dead time \n
+    """
+
+    # Create initial guess
+    trueRate = obsCountRate
+    n = int(deadTime/float(beamPeriod))
+    tmpObsRate = (1-exp(-trueRate*beamPeriod))/float(beamPeriod)\
+                 /(1+(1-exp(-trueRate*beamPeriod))*n)
+
+    # Solve iteratively
+    while abs(tmpObsRate - obsCountRate) > 1:
+        trueRate += 1
+        tmpObsRate = (1-exp(-trueRate*beamPeriod))/float(beamPeriod)\
+                     /(1+(1-exp(-trueRate*beamPeriod))*n)
+
+    deadTime = (trueRate-obsCountRate)/float(trueRate)
+    return trueRate, deadTime
+
+#------------------------------------------------------------------------------#
+def paralyzable_beam_dead_time(obsCountRate, deadTime, beamPeriod):
+    """!
+    @ingroup Detectors
+    Calculates the true count rate and dead time fraction using a paralyzable
+    model given a measured count rate and the known system dead time according
+    to the bunched synchrotron radiation dead time model from: "Success and
+    failure of dead- time models as applied to hybrid pixel detectors in
+    high-flux applications."
+
+    \f$ N_{out} = N_{in}* \exp(-N_{in}* \tau_b*(2n+1)) \f$
+
+    where
+
+    \f$ n=Int(\frac{\tau_s}{\tau_b})\f$
+
+    This is solved iteratively since \f$N_{in}\f$ cannot be solved for
+    exlicitly.
+
+    @param obsCountRate: <em> integer or float </em> \n
+        \f$N_{out}\f$: The recorded count rate for the system in units of
+        [\f$s^{-1}\f$] \n
+    @param deadTime: <em> integer or float </em>  \n
+        \f$\tau_s\f$: System dead time in untis of [s]  \n
+    @param beamPeriod: <em> integer or float </em>  \n
+        \f$\tau_b\f$: The time between beam bunches in untis of [s]  \n
+
+    @return float: \f$N_{in}\f$: The actual interaction rate \n
+            float: The fractional dead time \n
+    """
+
+    # Create initial guess
+    trueRate = obsCountRate
+    n = int(deadTime/float(beamPeriod))
+
+    # Solve iteratively
+    while abs(trueRate*exp(-trueRate*beamPeriod*(n+1)) - obsCountRate) > 1:
+        trueRate += 1
+
+    deadTime = (trueRate-obsCountRate)/float(trueRate)
+    return trueRate, deadTime
